8cf252e931699d16a33e6c0ab75a18b56797c560
T-Wesst <code.daddy17@gmail.com>
2018-11-06 16:29:47 -0800

    rm colon

--- selectNobel.sql
+++ selectNobel.sql
@@ -60,4 +60,4 @@ ORDER BY yr DESC, winner
 -- #14 The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1. Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
 SELECT winner, subject FROM nobel
 WHERE yr = 1984
-ORDER BY subject IN ('Chemistry', 'Physics'), subject, winner;
+ORDER BY subject IN ('Chemistry', 'Physics'), subject, winner
